8ceb673ba77c56accda43860a24bff83728b8947
zadolbali10 <anon.yu@mail.ru>
2022-09-18 18:01:25 +0300

    Initial commit

---
+++ Client/Client.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <boost/asio.hpp>
+
+namespace asio = boost::asio;
+
+int main(int argc, char** argv)
+{
+    asio::io_service ios;
+    asio::ip::tcp::socket socket(ios);
+
+    // Socket opening
+    try
+    {
+        socket.open(asio::ip::tcp::v4());
+        std::cout << "Successfull opening of a socket!" << std::endl;
+    }
+    catch (boost::system::system_error& error)
+    {
+        std::cout << error.code() << " " << error.what() << std::endl;
+        std::getchar();
+        return 0;
+    }
+
+    std::string rawIp = "127.0.0.1";
+    u_short port = 3333;
+    asio::ip::address_v4 address;
+
+    // Address parsing
+    try
+    {
+        address = asio::ip::address_v4::from_string(rawIp);
+    }
+    catch (boost::system::system_error& error)
+    {
+        std::cout << error.code() << " " << error.what() << std::endl;
+        std::getchar();
+        return 0;
+    }
+
+    asio::ip::tcp::endpoint endpoint(address, port);
+
+    // Connecting a socket to an endpoint
+    try
+    {
+        socket.connect(endpoint);
+        std::cout << "Successfull connection to a server!" << std::endl;
+
+        asio::write(socket, asio::buffer("Hello, server!\nI am a client!!!!"));
+    }
+    catch (boost::system::system_error& error)
+    {
+        std::cout << error.code() << " " << error.what() << std::endl;
+        std::getchar();
+        return 0;
+    }
+}
---
+++ Server/Server.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <boost/asio.hpp>
+
+namespace asio = boost::asio;
+
+std::string read(asio::ip::tcp::socket& socket)
+{
+    std::string result;
+    asio::streambuf buf;
+
+    try
+    {
+        asio::read(socket, buf);
+    }
+    catch (boost::system::system_error&)
+    {
+        std::istream input(&buf);
+        result = std::string(std::istreambuf_iterator<char>(input), {});
+    }
+
+    return result;
+}
+
+int main(int argc, char** argv)
+{
+    asio::io_service ios;
+    asio::ip::tcp::acceptor acceptor(ios);
+
+    // Acceptor openning
+    try
+    {
+        acceptor.open(asio::ip::tcp::v4());
+        std::cout << "Successfull opening of an acceptor!" << std::endl;
+    }
+    catch (boost::system::system_error& error)
+    {
+        std::cout << error.code() << " " << error.what() << std::endl;
+        std::getchar();
+        return 0;
+    }
+
+    u_short port = 3333;
+    asio::ip::tcp::endpoint endpoint(asio::ip::address_v4::any(), port);
+
+    // Binding an endpoint to an acceptor
+    try
+    {
+        acceptor.bind(endpoint);
+        std::cout << "Successfull binding of an endpoint!" << std::endl;
+    }
+    catch (boost::system::system_error& error)
+    {
+        std::cout << error.code() << " " << error.what() << std::endl;
+        std::getchar();
+        return 0;
+    }
+
+    // Listening and ascepting clients
+    try
+    {
+        acceptor.listen(10);
+        std::cout << "Successfull started listening!" << std::endl;
+
+        asio::ip::tcp::socket client(ios);
+        acceptor.accept(client);
+        std::cout << "Accepted a client from " << client.local_endpoint().address() << std::endl;
+
+        std::string message = read(client);
+
+        std::cout << "Client's message: " << message << std::endl;
+        std::getchar();
+    }
+    catch (boost::system::system_error& error)
+    {
+        std::cout << error.code() << " " << error.what() << std::endl;
+        std::getchar();
+        return 0;
+    }
+}
